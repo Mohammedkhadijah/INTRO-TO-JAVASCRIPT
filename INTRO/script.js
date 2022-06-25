0a4cfe89a7c718a83ee56a1eeba0b9a874cbbf52
Mohammedkhadijah <105322788+Mohammedkhadijah@users.noreply.github.com>
2022-06-25 13:50:00 +0100

    Add files via upload

---
+++ INTRO/script.js
@@ -0,0 +1,8 @@
+const info = {
+    name: 'Khadijah',
+    height:  `5'7`,
+    country: 'Nigeria',
+};
+alert(
+    `Hello there!!,my Name is ${info.name}, I'am ${info.height} tall and I'am from ${info.country}`  
+)
